3387d29f01605b9802bd9f7c36b43ccf99f7c1cb
Gustavo <gustavomoraessss93@hotmail.com>
2024-01-14 02:13:23 -0300

    Solution of error about the ESLint on project and addition of functions on firebase index.js file

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,3 +1,5 @@
+
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -9,6 +11,7 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +20,11 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.createChatEngineUser = functions.auth.user().onCreate((user) => {
+    console.log("create", user);
+  });
+  
+  exports.deleteChatEngineUser = functions.auth.user().onDelete((user) => {
+    console.log("delete", user);
+  });
